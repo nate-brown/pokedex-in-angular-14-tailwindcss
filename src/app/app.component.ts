62ada63b1ad0f9b22dc0149573825f72095dedea
nate-brown <31903179+nate-brown@users.noreply.github.com>
2022-10-18 16:30:34 +0200

    build(project): install tailwind css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pokedex-in-angular-14-tailwindcss';
+    title = 'pokedex-in-angular-14-tailwindcss';
 }
